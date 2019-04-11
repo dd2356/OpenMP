19a3bb067cc05f1b55ecf76a1045f11f6375a7d1
Kristian <krijor@kth.se>
2019-04-11 18:57:02 +0200

    Finished part of exercise 2

--- OpenMP/exercise_2/src/sum.c
+++ OpenMP/exercise_2/src/sum.c
@@ -2,27 +2,66 @@
 
 void omp_sum(double *sum_ret)
 {
-
+	double sum = 0;
+    #pragma omp parallel for
+	for (int i = 0; i < size; i++){
+		sum += x[i];
+	}
+	*sum_ret = sum;
 }
 
 void omp_critical_sum(double *sum_ret)
 {
-
+	double sum = 0;
+    #pragma omp parallel for
+	for (int i = 0; i < size; i++){
+        #pragma omp critical
+		sum += x[i];
+	}
+	*sum_ret = sum;
 }
 
 void omp_atomic_sum(double *sum_ret)
 {
-
+	double sum = 0;
+    #pragma omp parallel for
+	for (int i = 0; i < size; i++){
+        #pragma omp atomic
+		sum += x[i];
+	}
+	*sum_ret = sum;
 }
 
 void omp_local_sum(double *sum_ret)
 {
-
+    int threads = omp_get_max_threads(); 
+    double sum[threads]; 
+    for (int i = 0; i < threads; i++) sum[i]=0; 
+    #pragma omp parallel for
+    for (int i = 0; i < size; i++){
+        int tid = omp_get_thread_num(); 
+        sum[tid] += x[i];
+    }
+    *sum_ret = 0; 
+    for (int i = 0; i < threads; i++) {
+        *sum_ret += sum[i];
+    }
 }
 
 void omp_padded_sum(double *sum_ret)
 {
-
+    int threads = omp_get_max_threads(); 
+    double sum[threads*8]; 
+    for (int i = 0; i < threads; i++) sum[i*8]=0; 
+    #pragma omp parallel for
+    for (int i = 0; i < size; i++){
+        int tid = omp_get_thread_num(); 
+        sum[tid*8] += x[i];
+    }
+    *sum_ret = 0; 
+    for (int i = 0; i < threads; i++) {
+        *sum_ret += sum[i*8];
+    }
 }
 
 void omp_private_sum(double *sum_ret)
